ba490272a625a73614bc4a9452a274ce9794a671
m3mm3d0fficial <zaka-mammadov@unec.edu.az>
2024-07-14 14:28:15 +0400

    salam

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,120 @@ const moreBtn = document.querySelectorAll('.moreBtn')
 const usefulHead = document.querySelectorAll('.h2')
 const labels = document.querySelectorAll('.label')
 const tabs = document.querySelectorAll('.tabs button')
+const dateBtn = document.querySelector('#dateBtn')
+
+const destinations =[
+    {
+        city: "Bakı",
+        country: "Azərbaycan",
+        code: "GYD"
+    },
+    {
+        city: "Gəncə",
+        country: "Azərbaycan",
+        code: "KVD"
+    },
+    {
+        city: "Naxçıvan",
+        country: "Azərbaycan",
+        code: "NAJ"
+    },
+    {
+        city: "İstanbul",
+        country: "Türkiyə",
+        code: "IST"
+    },
+    {
+        city: "Ankara",
+        country: "Türkiyə",
+        code: "ESB"
+    },
+    {
+        city: "Moskva",
+        country: "Rusiya",
+        code: "DME"
+    },
+    {
+        city: "Sankt-Peterburq",
+        country: "Rusiya",
+        code: "LED"
+    },
+    {
+        city: "Dubai",
+        country: "Birləşmiş Ərəb Əmirlikləri",
+        code: "DXB"
+    },
+    {
+        city: "London",
+        country: "Birləşmiş Krallıq",
+        code: "LHR"
+    },
+    {
+        city: "Berlin",
+        country: "Almaniya",
+        code: "BER"
+    },
+    {
+        city: "Frankfurt",
+        country: "Almaniya",
+        code: "FRA"
+    },
+    {
+        city: "Paris",
+        country: "Fransa",
+        code: "CDG"
+    },
+    {
+        city: "Milano",
+        country: "İtaliya",
+        code: "MXP"
+    },
+    {
+        city: "Roma",
+        country: "İtaliya",
+        code: "FCO"
+    },
+    {
+        city: "Tel-Əviv",
+        country: "İsrail",
+        code: "TLV"
+    },
+    {
+        city: "Tbilisi",
+        country: "Gürcüstan",
+        code: "TBS"
+    },
+    {
+        city: "Nur-Sultan",
+        country: "Qazaxıstan",
+        code: "NQZ"
+    },
+    {
+        city: "Daşkənd",
+        country: "Özbəkistan",
+        code: "TAS"
+    },
+    {
+        city: "Dehli",
+        country: "Hindistan",
+        code: "DEL"
+    },
+    {
+        city: "Doha",
+        country: "Qətər",
+        code: "DOH"
+    },
+    {
+        city: "Vyana",
+        country: "Avstriya",
+        code: "VIE"
+    },
+    {
+        city: "Pekin",
+        country: "Çin",
+        code: "PEK"
+    }
+];
 
 
 
@@ -27,129 +141,204 @@ function tabsChange() {
 tabsChange();
 
 nightBtn.addEventListener('click', function () {
-    if(light.src.includes('light') ) {
+    if (light.src.includes('light')) {
         nightMode();
+    } else {
+        defaultMode();
     }
-    else{defaultMode();}
 })
 
 function nightMode() {
     light.src = 'assets/img/moon.svg'
     body.classList.add('bg-black')
     footer.classList.remove('bg-[#01357e]')
-    for(let btn of socialBtn) {
-        btn.classList.replace('bg-white','bg-[#1f2022]')
-        btn.classList.replace('text-[#01357e]','text-white')
+    for (let btn of socialBtn) {
+        btn.classList.replace('bg-white', 'bg-[#1f2022]')
+        btn.classList.replace('text-[#01357e]', 'text-white')
     }
     // for(let direct of directions) {
-        //     direct.classList.add('text-white')
-        // }
-        for(let useful of usefulLink) {
-            useful.classList.add('bg-[#1f2022]');
-            useful.classList.remove('border')
-        }
-        for(let btnMore of moreBtn) {
-            btnMore.classList.replace('text-[#40b7de]','text-white')
-        }
-    for(let head of usefulHead) {
+    //     direct.classList.add('text-white')
+    // }
+    for (let useful of usefulLink) {
+        useful.classList.add('bg-[#1f2022]');
+        useful.classList.remove('border')
+    }
+    for (let btnMore of moreBtn) {
+        btnMore.classList.replace('text-[#40b7de]', 'text-white')
+    }
+    for (let head of usefulHead) {
         head.classList.add('text-white')
     }
-    
-    
+
+
 
 }
+
 function defaultMode() {
     light.src = 'assets/img/light.svg'
     body.classList.remove('bg-black')
     footer.classList.add('bg-[#01357e]')
-    for(let btn of socialBtn) {
-        btn.classList.replace('bg-[#1f2022]','bg-white')
-        btn.classList.replace('text-white','text-[#01357e]')
+    for (let btn of socialBtn) {
+        btn.classList.replace('bg-[#1f2022]', 'bg-white')
+        btn.classList.replace('text-white', 'text-[#01357e]')
     }
     // for(let direct of directions) {
-        //     direct.classList.remove('text-white')
-        // } 
-        for(let useful of usefulLink) {
-            useful.classList.remove('bg-[#1f2022]');
-            useful.classList.add('border')
-        }
-        for(let btnMore of moreBtn) {
-            btnMore.classList.replace('text-white','text-[#40b7de]')
-        }
-        for(let head of usefulHead) {
-            head.classList.remove('text-white')
-        }
+    //     direct.classList.remove('text-white')
+    // } 
+    for (let useful of usefulLink) {
+        useful.classList.remove('bg-[#1f2022]');
+        useful.classList.add('border')
     }
+    for (let btnMore of moreBtn) {
+        btnMore.classList.replace('text-white', 'text-[#40b7de]')
+    }
+    for (let head of usefulHead) {
+        head.classList.remove('text-white')
+    }
+}
+
+
+function filterOptions(input, dropdown) {
+    const value = input.value.toLowerCase();
+    const filteredDestinations = destinations.filter(destination => 
+        destination.city.toLowerCase().includes(value) || 
+        destination.code.toLowerCase().includes(value)
+    );
     
-    function inputValueCheck() {
-        const inputs = document.querySelectorAll('.from');
-        
-        for (let input of inputs) {
-            const label = input.nextElementSibling;
-            const border = input.closest('.labell');
-            const dropdown = input.closest('.labell').nextElementSibling;
+    dropdown.innerHTML = "";
     
-            input.addEventListener('focus', function () {
+    filteredDestinations.forEach(destination => {
+        const option = document.createElement('div');
+        option.className = 'option cursor-pointer hover:bg-[#eeeeee80]';
+        option.innerHTML = `
+            <button class="p-[16px] flex items-center text-[14px] font-semibold leading-[20px] w-full">
+                <div class="flex items-center w-full">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="text-[#CDD5DF] mr-[10px]">
+                        <path d="M5.19593 2.6236C5.0576 2.62532 4.91992 2.66184 4.79554 2.73184C4.32471 2.99725 4.22501 3.63237 4.59209 4.02904L8.09307 7.8124L4.88831 9.21296L2.91484 8.05491C2.67109 7.912 2.37143 7.90298 2.11976 8.03131C1.68518 8.25215 1.53424 8.79964 1.79424 9.21214L2.91078 10.9838C3.53161 11.9688 4.77949 12.3521 5.84616 11.8855L17.4868 6.79108C18.2014 6.47858 18.5272 5.64531 18.2151 4.93073C17.9022 4.21615 17.0694 3.88988 16.3548 4.20238L11.9782 6.11481L5.59876 2.72126C5.47272 2.65438 5.33427 2.62188 5.19593 2.6236ZM3.12481 15.8332C3.04199 15.8321 2.95976 15.8474 2.8829 15.8782C2.80605 15.9091 2.7361 15.955 2.67712 16.0131C2.61814 16.0713 2.5713 16.1406 2.53933 16.217C2.50737 16.2934 2.4909 16.3754 2.4909 16.4582C2.4909 16.5411 2.50737 16.6231 2.53933 16.6995C2.5713 16.7759 2.61814 16.8452 2.67712 16.9033C2.7361 16.9615 2.80605 17.0073 2.8829 17.0382C2.95976 17.0691 3.04199 17.0844 3.12481 17.0832H16.8748C16.9576 17.0844 17.0399 17.0691 17.1167 17.0382C17.1936 17.0073 17.2635 16.9615 17.3225 16.9033C17.3815 16.8452 17.4283 16.7759 17.4603 16.6995C17.4922 16.6231 17.5087 16.5411 17.5087 16.4582C17.5087 16.3754 17.4922 16.2934 17.4603 16.217C17.4283 16.1406 17.3815 16.0713 17.3225 16.0131C17.2635 15.955 17.1936 15.9091 17.1167 15.8782C17.0399 15.8474 16.9576 15.8321 16.8748 15.8332H3.12481Z"></path>
+                    </svg>
+                    <span class="flex-1 text-left">${destination.city}, ${destination.country}</span>
+                    <span class="text-[#6E7583]">${destination.code}</span>
+                </div>
+            </button>
+        `;
+        dropdown.appendChild(option);
+
+        option.querySelector('button').addEventListener('click', () => {
+            input.value = destination.city + ", " + destination.country;
+            dropdown.classList.remove('show');
+        });
+    });
+}
+function inputValueCheck() {
+    const inputs = document.querySelectorAll('.from');
+    
+    for (let input of inputs) {
+        const label = input.nextElementSibling;
+        const border = input.closest('.labell');
+        const dropdown = input.closest('.labell').nextElementSibling;
+
+        input.addEventListener('focus', function () {
+            label.classList.add('focused');
+            border.style.borderColor = '#37A6DB';
+            dropdown.classList.add('show');
+            filterOptions(input, dropdown); // İlk dəfə göstərmək üçün
+        });
+
+        input.addEventListener('blur', function () {
+            setTimeout(() => {
+                dropdown.classList.remove('show');
+                if (input.value === '') {
+                    label.classList.remove('focused');
+                    border.style.borderColor = '#C52F5C';
+                } else {
+                    border.style.borderColor = '#DBE0E4'; // Default boz rəng
+                }
+            }, 200); // Bu gecikmə, dropdown seçimlərindəki klik hadisəsinin qeydiyyata alınmasına imkan verir
+        });
+
+        input.addEventListener('input', function (event) {
+            const value = event.target.value;
+
+            if (value !== '') {
                 label.classList.add('focused');
                 border.style.borderColor = '#37A6DB';
                 dropdown.classList.add('show');
-            });
-    
-            input.addEventListener('blur', function () {
-                setTimeout(() => {
-                    dropdown.classList.remove('show');
-                    if (input.value === '') {
-                        label.classList.remove('focused');
-                        border.style.borderColor = '#C52F5C';
-                    } else {
-                        border.style.borderColor = '#DBE0E4'; // Default boz rəng
-                    }
-                }, 200); // Bu gecikmə, dropdown seçimlərindəki klik hadisəsinin qeydiyyata alınmasına imkan verir
-            });
-    
-            input.addEventListener('input', function (event) {
-                const value = event.target.value;
-    
-                if (value !== '') {
-                    label.classList.add('focused');
-                    border.style.borderColor = '#37A6DB';
-                } else {
-                    label.classList.add('focused');
-                    border.style.borderColor = '#37A6DB';
-                }
-            });
-    
-            dropdown.querySelectorAll('.option button').forEach(button => {
-                button.addEventListener('click', function () {
-                    const countryName = this.querySelector('.flex-1').textContent;
-                    input.value = countryName;
-                    dropdown.classList.remove('show');
-                });
-            });
-        }
-    }
-    
-    function setupChangeButton() {
-        const changeBtn = document.getElementById('changeBtn');
-        const inputs = document.querySelectorAll('.from');
-    
-        changeBtn.addEventListener('click', function() {
-            if (inputs[0].value !== '' && inputs[1].value !== '') {
-                const tempValue = inputs[0].value;
-                inputs[0].value = inputs[1].value;
-                inputs[1].value = tempValue;
+                filterOptions(input, dropdown);
+            } else {
+                label.classList.add('focused');
+                border.style.borderColor = '#37A6DB';
+                filterOptions(input, dropdown); // Boş olduqda da göstərmək üçün
             }
         });
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        inputValueCheck();
-        setupChangeButton();
-    });
-    
-    
-    
-       
 
+        dropdown.querySelectorAll('.option button').forEach(button => {
+            button.addEventListener('click', function () {
+                const countryName = this.querySelector('.flex-1').textContent;
+                input.value = countryName;
+                dropdown.classList.remove('show');
+            });
+        });
+    }
+}
+
+function setupChangeButton() {
+    const changeBtn = document.getElementById('changeBtn');
+    const inputs = document.querySelectorAll('.from');
+
+    changeBtn.addEventListener('click', function () {
+        if (inputs[0].value !== '' && inputs[1].value !== '') {
+            const tempValue = inputs[0].value;
+            inputs[0].value = inputs[1].value;
+            inputs[1].value = tempValue;
+        }
+    });
+}
+
+dateBtn.addEventListener('click', function () {
+    const nextDiv = dateBtn.nextElementSibling;
+    nextDiv.classList.toggle('hidden')
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    inputValueCheck();
+    setupChangeButton();
+
+    const buttons = document.querySelectorAll('#monthBtn button');
+
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => {
+                btn.classList.remove('text-[#2C8DC7]', 'font-semibold', 'active');
+                btn.classList.add('text-black');
+                if (btn.querySelector('.line')) {
+                    btn.querySelector('.line').remove();
+                }
+            });
+
+            button.classList.add('text-[#2C8DC7]', 'font-semibold', 'active');
+            button.classList.remove('text-black');
+            if (!button.querySelector('.line')) {
+                const lineDiv = document.createElement('div');
+                lineDiv.className = 'line absolute w-[4px] h-[32px] bg-[#2C8DC7] left-0';
+                button.prepend(lineDiv);
+            }
+
+            if (index + 1 < buttons.length) {
+                const nextButton = buttons[index + 1];
+                nextButton.classList.add('text-[#2C8DC7]', 'font-semibold', 'active');
+                nextButton.classList.remove('text-black');
+                if (!nextButton.querySelector('.line')) {
+                    const lineDiv = document.createElement('div');
+                    lineDiv.className = 'line absolute w-[4px] h-[32px] bg-[#2C8DC7] left-0';
+                    nextButton.prepend(lineDiv);
+                }
+            }
+        });
+    });
+
+
+    
+
+});
 
 
